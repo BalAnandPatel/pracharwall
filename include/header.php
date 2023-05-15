971526e5398a8099cafa8e2b9e3a674a4eefb14b
YashMaury <yasmaurya2000@gmail.com>
2023-05-15 17:34:16 +0530

    pracharwall

---
+++ include/header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pracharwall</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <nav class="navbar sticky-top navbar-expand-md navbar-light bg-light" aria-label="Navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index1.php">
+                <img src="Pracharwall_image/logo.png" alt="Pracharwall" width="160" height="40">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04"
+                aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div>
+                <div class="collapse navbar-collapse" id="navbarsExample04">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="register-wall.php">Register Your Wall</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="bi bi-file-arrow-down-fill"></i> Download App</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="sign-in.php">Sign in</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="sign-up.php">Sign up</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
---
+++ sign-up.php
@@ -0,0 +1,204 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Pracharwall</title>
+</head>
+
+<body>
+
+
+    <div class="d-flex">
+
+        <!-- Section: Design Block -->
+        <section class="text-center text-lg-start col-6">
+            <style>
+                .cascading-right {
+                    margin-right: -50px;
+                }
+
+                @media (max-width: 991.98px) {
+                    .cascading-right {
+                        margin-right: 0;
+                    }
+                }
+            </style>
+
+
+
+            <!-- Jumbotron -->
+            <div class="container py-4">
+                <div class="row g-0 align-items-center">
+                    <div class="col-lg-8 mb-5 mb-lg-0">
+                        <div class="card cascading-right" style="
+              background: hsla(0, 0%, 100%, 0.55);
+              backdrop-filter: blur(30px);
+              ">
+                            <div class="card-body p-5 shadow-5 text-center">
+                                <h2 class="fw-bold mb-5">Sign up now</h2>
+                                <form>
+                                    <!-- 2 column grid layout with text inputs for the first and last names -->
+                                    <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-outline">
+                                                <input type="text" id="form3Example1" class="form-control" />
+                                                <label class="form-label" for="form3Example1">First name</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-outline">
+                                                <input type="text" id="form3Example2" class="form-control" />
+                                                <label class="form-label" for="form3Example2">Last name</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- Email input -->
+                                    <div class="form-outline mb-4">
+                                        <input type="email" id="form3Example3" class="form-control" />
+                                        <label class="form-label" for="form3Example3">Email address</label>
+                                    </div>
+
+                                    <!-- Password input -->
+                                    <div class="form-outline mb-4">
+                                        <input type="password" id="form3Example4" class="form-control" />
+                                        <label class="form-label" for="form3Example4">Password</label>
+                                    </div>
+
+                                    <!-- Checkbox -->
+                                    <!-- <div class="d-flex justify-content-center">
+                                        <div class="form-check d-flex mb-4">
+                                            <input class="form-check-input me-2" type="checkbox" value=""
+                                                id="form2Example33" checked />
+                                            <label class="form-check-label" for="form2Example33">
+                                                Subscribe to our newsletter
+                                            </label>
+                                        </div>
+                                    </div> -->
+
+                                    <!-- Submit button -->
+                                    <button type="submit" class="btn btn-primary btn-block mb-4">
+                                        Sign up
+                                    </button>
+
+                                    <!-- Register buttons -->
+                                    <!-- <div class="text-center">
+                                        <p>or sign up with:</p>
+                                        <button type="button" class="btn btn-link btn-floating mx-1">
+                                            <i class="fab fa-facebook-f"></i>
+                                        </button>
+
+                                        <button type="button" class="btn btn-link btn-floating mx-1">
+                                            <i class="fab fa-google"></i>
+                                        </button>
+
+                                        <button type="button" class="btn btn-link btn-floating mx-1">
+                                            <i class="fab fa-twitter"></i>
+                                        </button>
+
+                                        <button type="button" class="btn btn-link btn-floating mx-1">
+                                            <i class="fab fa-github"></i>
+                                        </button>
+                                    </div> -->
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!-- Jumbotron -->
+        </section>
+        <!-- Section: Design Block -->
+
+
+
+        <div class="row col-6">
+            <div class="col-md-12 mb-4">
+                <div class="card mb-4">
+                    <div class="card-header py-3">
+                        <h5 class="mb-0">Address details</h5>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <!-- 2 column grid layout with text inputs for the first and last names -->
+                            <div class="row mb-4">
+                                <div class="col">
+                                    <div class="form-outline">
+                                        <input type="text" id="form7Example1" class="form-control" />
+                                        <label class="form-label" for="form7Example1">First name</label>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-outline">
+                                        <input type="text" id="form7Example2" class="form-control" />
+                                        <label class="form-label" for="form7Example2">Last name</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Text input -->
+                            <div class="form-outline mb-4">
+                                <input type="text" id="form7Example3" class="form-control" />
+                                <label class="form-label" for="form7Example3">Company name</label>
+                            </div>
+
+                            <!-- Text input -->
+                            <div class="form-outline mb-4">
+                                <input type="text" id="form7Example4" class="form-control" />
+                                <label class="form-label" for="form7Example4">Address</label>
+                            </div>
+
+                            <!-- Email input -->
+                            <div class="form-outline mb-4">
+                                <input type="email" id="form7Example5" class="form-control" />
+                                <label class="form-label" for="form7Example5">Email</label>
+                            </div>
+
+                            <!-- Number input -->
+                            <div class="form-outline mb-4">
+                                <input type="number" id="form7Example6" class="form-control" />
+                                <label class="form-label" for="form7Example6">Phone</label>
+                            </div>
+
+                            <!-- Message input -->
+                            <div class="form-outline mb-2">
+                                <textarea class="form-control" id="form7Example7" rows="4"></textarea>
+                                <label class="form-label" for="form7Example7">Additional information</label>
+                            </div>
+
+                            <!-- Checkbox -->
+                            <!-- <div class="d-flex justify-content-center">
+                                <div class="form-check d-flex mb-2">
+                                    <input class="form-check-input me-2" type="checkbox" value="" id="form7Example8"
+                                        checked />
+                                    <label class="form-check-label" for="form7Example8">
+                                        Create an account?
+                                    </label>
+                                </div>
+                            </div> -->
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
+
+    </div>
+
+</body>
+
+</html>
