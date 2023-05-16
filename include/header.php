3b1bdd222cf09b169f8e0735cbfc5f614a8cee79
YashMaury <yasmaurya2000@gmail.com>
2023-05-16 17:25:16 +0530

    upd

--- include/header.php
+++ include/header.php
@@ -8,6 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <script src="search.js"></script>
 </head>
 
 <body>
@@ -25,6 +29,16 @@
                 <div class="collapse navbar-collapse" id="navbarsExample04">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                         <li class="nav-item">
+                            <div class="dropdown">
+                                <input type="text" id="ser-city" data-bs-toggle="dropdown" aria-expanded="false"
+                                    class="form-control" placeholder="Search Your City"
+                                    onkeyup="showResult(this.value)" />
+                                <ul class="dropdown-menu">
+                                    <li><div id="livesearch"></div></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="register-wall.php">Register Your Wall</a>
                         </li>
                         <li class="nav-item">
@@ -36,8 +50,88 @@
                         <li class="nav-item">
                             <a class="nav-link" href="sign-up.php">Sign up</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal" href="">Sign
+                                up</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="modal" data-bs-target="#signin" href="">Sign
+                                in</a>
+                        </li>
                     </ul>
                 </div>
             </div>
         </div>
     </nav>
+
+
+    <!-- Modal Sign Up-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header text-center">
+                    <h4 class="modal-title w-100 font-weight-bold">Sign up</h4>
+                    <!-- <button type="button" class="btn" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button> -->
+                </div>
+                <div class="modal-body mx-3">
+                    <form>
+                        <div class="form-group">
+                            <label for="username">Full Name:</label>
+                            <input type="text" class="form-control" id="username">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="email">Email address:</label>
+                            <input type="email" class="form-control" id="email">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="pwd">Password:</label>
+                            <input type="password" class="form-control" id="pwd">
+                        </div>
+                        <br>
+                    </form>
+                </div>
+                <div class="modal-footer d-flex justify-content-center">
+                    <button class="btn btn-default">Register</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+
+    <!-- Modal Sign Up-->
+    <div class="modal fade" id="signin" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header text-center">
+                    <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+                    <!-- <button type="button" class="btn" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button> -->
+                </div>
+                <div class="modal-body mx-3">
+                    <form>
+                        <div class="form-group">
+                            <label for="email">Email address:</label>
+                            <input type="email" class="form-control" id="email">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="pwd">Password:</label>
+                            <input type="password" class="form-control" id="pwd">
+                        </div>
+                        <br>
+                    </form>
+                </div>
+                <div class="modal-footer d-flex justify-content-center">
+                    <button class="btn btn-default">Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
--- register-wall.php
+++ register-wall.php
@@ -2,154 +2,134 @@
 include "include/header.php";
 ?>
 
-<script>
-    function showResult(str) {
-        if (str.length == 0) {
-            document.getElementById("livesearch").innerHTML = "";
-            document.getElementById("livesearch").style.border = "0px";
-            return;
-        }
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("livesearch").innerHTML = this.responseText;
-                document.getElementById("livesearch").style.border = "1px solid #A5ACB2";
-            }
-        }
-        xmlhttp.open("GET", "livesearch.php?q=" + str, true);
-        xmlhttp.send();
-    }
-</script>
-</head>
 
-<body>
-
-
-    <div class="container mt-4">
-        <form>
-            <!-- 2 column grid layout with text inputs for the first and last names -->
-            <div class="row mb-4">
-                <div class="col">
-                    <div class="form-outline">
-                        <input type="text" id="ser-name" class="form-control" />
-                        <label class="form-label" for="ser-name">Service Name</label>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="form-outline">
-                        <input type="text" id="ser-type" class="form-control" />
-                        <label class="form-label" for="ser-type">Service Type</label>
-                    </div>
+<center class="pt-2">
+    <h2>Register Your Wall</h2>
+</center>
+<div class="container mt-4 w-50 border p-4 border-secondary rounded">
+    <form>
+        <!-- 2 column grid layout with text inputs for the first and last names -->
+        <div class="row mb-4">
+            <div class="col">
+                <div class="form-outline">
+                    <input type="text" id="ser-name" class="form-control" />
+                    <label class="form-label" for="ser-name">Service Name</label>
                 </div>
             </div>
-
-            <!-- File input -->
-            <div class="form-outline mb-4">
-                <input type="file" id="ser-file" class="form-control" />
-                <label class="form-label" for="ser-file">Service Demo File</label>
-            </div>
-
-            <!-- Email input -->
-            <div class="form-outline mb-4">
-                <input type="email" id="form6Example5" class="form-control" />
-                <label class="form-label" for="form6Example5">Email</label>
-            </div>
-
-            <!-- Text input -->
-            <div class="row mb-4">
-                <div class="col">
-                    <div class="form-outline">
-                        <input type="text" id="ser-city" class="form-control" onkeyup="showResult(this.value)" />
-                        <label class="form-label" for="ser-city">City</label>
-                        <div id="livesearch"></div>
-                    </div>
-                </div>
-
-                <!-- Number input -->
-                <div class="col">
-                    <div class="form-outline">
-                        <input type="number" id="form6Example6" class="form-control" />
-                        <label class="form-label" for="form6Example6">Mobile No.</label>
-                    </div>
+            <div class="col">
+                <div class="form-outline">
+                    <input type="text" id="ser-type" class="form-control" />
+                    <label class="form-label" for="ser-type">Service Type</label>
                 </div>
             </div>
-
-            <!-- Message input -->
-            <div class="form-outline mb-4">
-                <textarea class="form-control" id="form6Example7" rows="4"></textarea>
-                <label class="form-label" for="form6Example7">Service Description</label>
-            </div>
-
-            <!-- Submit button -->
-            <button type="submit" class="btn btn-primary btn-block mb-4">Register Wall</button>
-        </form>
-    </div>
-    <br>
-    <div style="background-color: #eee;">
-        <br>
-        <center>
-            <h1>Frequently Asked Questions</h1>
-        </center>
-        <br>
-
-        <div class="container">
-
-            <div class="card mb-3" style="width: 100%;">
-                <div class="card-body">
-                    <h5 class="card-title">How does it work on Pracharwall?</h5>
-                    <p class="card-text">First, we find customers who looking for help and collect information & verify
-                        their requirements. Pracharwall's matchmaking platform identifies
-                        relevant businesses based on the service type, location preference, and other factors. Once the
-                        match is found, we send the customers information to the matched businesses.
-                        The businesses can contact the customers directly and convert them to customers.
-                        Pracharwall will also help you build your brand by enriching your business profile with customer
-                        reviews and portfolios to help attract more customers to your business.
-                    </p>
-                </div>
-            </div>
-
-            <div class="card mb-3" style="width: 100%;">
-                <div class="card-body">
-                    <h5 class="card-title">Why should I register my business on Pracharwall?</h5>
-                    <p class="card-text">Today, the market is seeing cut-throat competition between small businesses
-                        while big companies are attracting customers with their marketing resources. Pracharwall is the best
-                        option for small business owners to get genuine leads and grow their business into a brand. Sign
-                        up today by clicking on the link below.
-                    </p>
-                </div>
-            </div>
-
-            <div class="card mb-3" style="width: 100%;">
-                <div class="card-body">
-                    <h5 class="card-title">How much do leads cost?</h5>
-                    <p class="card-text">The price of our service requests varies by the type of request and the
-                        location of the request. Once enrolled, you can see the current price of leads for each service
-                        request by downloading the Pracharwall Business app. To download the app click here.
-                    </p>
-                </div>
-            </div>
-
-            <div class="card mb-3" style="width: 100%;">
-                <div class="card-body">
-                    <h5 class="card-title">How much control do I have over the leads I receive?</h5>
-                    <p class="card-text">Pracharwall's business app gives you full control.
-                        You can define your services in-depth, locality preferences to ensure you receive leads matched
-                        as you want. If you are busy you can pause the lead flow and restart anytime. Sign up today to
-                        learn more!
-                    </p>
-                </div>
-            </div>
-
-            <div class="card mb-3" style="width: 100%;">
-                <div class="card-body">
-                    <h5 class="card-title">How can I list my business on Pracharwall?</h5>
-                    <p class="card-text">To begin your sign up, just click on the sign-up button below</p>
-                </div>
-            </div>
-
-            <br>
-
         </div>
-    </div>
 
+        <!-- File input -->
+        <div class="form-outline mb-4">
+            <input type="file" id="ser-file" class="form-control" />
+            <label class="form-label" for="ser-file">Service Demo File</label>
+        </div>
+
+        <!-- Email input -->
+        <div class="form-outline mb-4">
+            <input type="email" id="form6Example5" class="form-control" />
+            <label class="form-label" for="form6Example5">Email</label>
+        </div>
+
+        <!-- Text input -->
+        <div class="row mb-4">
+            <div class="col">
+                <div class="form-outline">
+                    <input type="text" id="city" class="form-control" />
+                    <label class="form-label" for="city">City</label>
+                </div>
+            </div>
+
+            <!-- Number input -->
+            <div class="col">
+                <div class="form-outline">
+                    <input type="number" id="form6Example6" class="form-control" />
+                    <label class="form-label" for="form6Example6">Mobile No.</label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Message input -->
+        <div class="form-outline mb-4">
+            <textarea class="form-control" id="form6Example7" rows="4"></textarea>
+            <label class="form-label" for="form6Example7">Service Description</label>
+        </div>
+
+        <!-- Submit button -->
+        <button type="submit" class="btn btn-primary btn-block mb-4">Register Wall</button>
+    </form>
+</div>
+<br>
+<div style="background-color: #eee;">
     <br>
+    <center>
+        <h1>Frequently Asked Questions</h1>
+    </center>
+    <br>
+
+    <div class="container">
+
+        <div class="card mb-3" style="width: 100%;">
+            <div class="card-body">
+                <h5 class="card-title">How does it work on Pracharwall?</h5>
+                <p class="card-text">First, we find customers who looking for help and collect information & verify
+                    their requirements. Pracharwall's matchmaking platform identifies
+                    relevant businesses based on the service type, location preference, and other factors. Once the
+                    match is found, we send the customers information to the matched businesses.
+                    The businesses can contact the customers directly and convert them to customers.
+                    Pracharwall will also help you build your brand by enriching your business profile with customer
+                    reviews and portfolios to help attract more customers to your business.
+                </p>
+            </div>
+        </div>
+
+        <div class="card mb-3" style="width: 100%;">
+            <div class="card-body">
+                <h5 class="card-title">Why should I register my business on Pracharwall?</h5>
+                <p class="card-text">Today, the market is seeing cut-throat competition between small businesses
+                    while big companies are attracting customers with their marketing resources. Pracharwall is the best
+                    option for small business owners to get genuine leads and grow their business into a brand. Sign
+                    up today by clicking on the link below.
+                </p>
+            </div>
+        </div>
+
+        <div class="card mb-3" style="width: 100%;">
+            <div class="card-body">
+                <h5 class="card-title">How much do leads cost?</h5>
+                <p class="card-text">The price of our service requests varies by the type of request and the
+                    location of the request. Once enrolled, you can see the current price of leads for each service
+                    request by downloading the Pracharwall Business app. To download the app click here.
+                </p>
+            </div>
+        </div>
+
+        <div class="card mb-3" style="width: 100%;">
+            <div class="card-body">
+                <h5 class="card-title">How much control do I have over the leads I receive?</h5>
+                <p class="card-text">Pracharwall's business app gives you full control.
+                    You can define your services in-depth, locality preferences to ensure you receive leads matched
+                    as you want. If you are busy you can pause the lead flow and restart anytime. Sign up today to
+                    learn more!
+                </p>
+            </div>
+        </div>
+
+        <div class="card mb-3" style="width: 100%;">
+            <div class="card-body">
+                <h5 class="card-title">How can I list my business on Pracharwall?</h5>
+                <p class="card-text">To begin your sign up, just click on the sign-up button below</p>
+            </div>
+        </div>
+
+        <br>
+
+    </div>
+</div>
+
+<br>
