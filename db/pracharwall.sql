dd2022e687ed8cbf58e73f746e47a904e4b0fa68
YashMaury <yasmaurya2000@gmail.com>
2023-05-30 14:06:32 +0530

    upd

---
+++ db/pracharwall.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 30, 2023 at 08:36 AM
+-- Server version: 5.6.20-log
+-- PHP Version: 5.4.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `pracharwall`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business_category`
+--
+
+CREATE TABLE IF NOT EXISTS `business_category` (
+`id` int(255) NOT NULL,
+  `businessCategory` varchar(100) NOT NULL,
+  `subCategory` varchar(100) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `createdOn` timestamp NOT NULL,
+  `createdBy` varchar(255) NOT NULL,
+  `updaredOn` timestamp NOT NULL,
+  `updatedBy` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_profile`
+--
+
+CREATE TABLE IF NOT EXISTS `user_profile` (
+`id` int(255) NOT NULL,
+  `userId` varchar(255) NOT NULL,
+  `businessCategory` varchar(100) NOT NULL,
+  `subCategory` varchar(100) NOT NULL,
+  `userAddress` varchar(200) NOT NULL,
+  `businessName` varchar(100) NOT NULL,
+  `establishmentYear` varchar(100) NOT NULL,
+  `paymentMode` varchar(100) NOT NULL,
+  `businessTiming` varchar(100) NOT NULL,
+  `userServices` varchar(100) NOT NULL,
+  `aboutUser` varchar(2000) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `createdOn` timestamp NOT NULL,
+  `createdBy` varchar(255) NOT NULL,
+  `updatedOn` timestamp NOT NULL,
+  `updatedBy` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_registration`
+--
+
+CREATE TABLE IF NOT EXISTS `user_registration` (
+  `id` int(255) NOT NULL,
+  `userType` varchar(100) NOT NULL,
+  `userName` varchar(100) NOT NULL,
+  `userMobile` varchar(12) NOT NULL,
+  `userEmail` varchar(200) NOT NULL,
+  `userPass` varchar(25) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `createdOn` timestamp NOT NULL,
+  `createdBy` varchar(255) NOT NULL,
+  `updatedOn` timestamp NOT NULL,
+  `updatedBy` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_type`
+--
+
+CREATE TABLE IF NOT EXISTS `user_type` (
+`id` int(255) NOT NULL,
+  `userType` varchar(100) NOT NULL,
+  `userRole` varchar(100) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `createdOn` timestamp NOT NULL,
+  `createdBy` varchar(255) NOT NULL,
+  `updatedOn` timestamp NOT NULL,
+  `updatedBy` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wall_uploads`
+--
+
+CREATE TABLE IF NOT EXISTS `wall_uploads` (
+`id` int(255) NOT NULL,
+  `userId` varchar(255) NOT NULL,
+  `businessCategory` varchar(100) NOT NULL,
+  `subCategory` varchar(100) NOT NULL,
+  `wallImg` varchar(200) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `createdOn` timestamp NOT NULL,
+  `createdBy` varchar(255) NOT NULL,
+  `updatedOn` timestamp NOT NULL,
+  `updatedBy` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `business_category`
+--
+ALTER TABLE `business_category`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_profile`
+--
+ALTER TABLE `user_profile`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_type`
+--
+ALTER TABLE `user_type`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `wall_uploads`
+--
+ALTER TABLE `wall_uploads`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `business_category`
+--
+ALTER TABLE `business_category`
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_profile`
+--
+ALTER TABLE `user_profile`
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_type`
+--
+ALTER TABLE `user_type`
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `wall_uploads`
+--
+ALTER TABLE `wall_uploads`
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
