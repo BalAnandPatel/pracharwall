14f7eed2980efeb36d97b94517c08cef8131a33b
Manu Computer <giplanand@gmail.com>
2023-06-03 17:19:55 +0530

    updats

--- db/pracharwall.sql
+++ db/pracharwall.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 02, 2023 at 07:52 AM
+-- Generation Time: Jun 03, 2023 at 11:48 AM
 -- Server version: 5.6.20-log
 -- PHP Version: 5.4.31
 
@@ -48,6 +48,23 @@ INSERT INTO `business_category` (`id`, `businessCategory`, `subCategory`, `statu
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `customer_inquiry`
+--
+
+CREATE TABLE IF NOT EXISTS `customer_inquiry` (
+`id` int(255) NOT NULL,
+  `userId` varchar(255) NOT NULL,
+  `cuName` varchar(100) NOT NULL,
+  `cuEmail` varchar(200) NOT NULL,
+  `requiredService` varchar(1000) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `createdOn` timestamp NOT NULL,
+  `createdBy` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user_profile`
 --
 
@@ -73,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `user_profile` (
   `createdBy` varchar(255) NOT NULL,
   `updatedOn` timestamp NOT NULL,
   `updatedBy` varchar(255) NOT NULL
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `user_profile`
@@ -81,7 +98,8 @@ CREATE TABLE IF NOT EXISTS `user_profile` (
 
 INSERT INTO `user_profile` (`id`, `userId`, `businessCategory`, `subCategory`, `alterMobile`, `userAddress`, `city`, `state`, `businessName`, `establishmentYear`, `paymentMode`, `businessTiming`, `businessDay`, `userServices`, `userWebsite`, `aboutUser`, `status`, `createdOn`, `createdBy`, `updatedOn`, `updatedBy`) VALUES
 (5, '4', 'Food', '', '3698574859', 'Pratapgarh Road Mungra Badshahpur Jaunpur 222202', 'Jaunpur', 'Uttar Pradesh', 'Shivam Restaurant ', '2023', 'Cash, Master Card, Debit Cards, Credit Cards', '10AM - 8PM', 'Mon - Sat', '', 'https://glintel.com/', 'Hi I am mrityunjay ', 1, '2023-05-31 20:39:58', 'Mrityunjay Singh', '2023-05-31 18:30:00', '4'),
-(6, '5', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 1, '2023-06-02 05:33:39', 'Karan', '0000-00-00 00:00:00', '');
+(6, '5', 'Realestate', '', '6354654646', 'Jhusi Prayagraj', 'Prayagraj', 'Uttar Pradesh', 'Ultrimax Land Developer', '2023', 'Cash, Master Card, Visa Card', '10AM - 8PM', 'Mon - Sat', '', 'https://glintel.com/', 'Ultrimax land developer a real estate company  ', 1, '2023-06-02 05:33:39', 'Karan', '2023-06-02 18:30:00', '5'),
+(7, '6', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, '2023-06-01 21:55:04', 'Rahul', '0000-00-00 00:00:00', '');
 
 -- --------------------------------------------------------
 
@@ -102,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `user_registration` (
   `createdBy` varchar(255) NOT NULL,
   `updatedOn` timestamp NOT NULL,
   `updatedBy` varchar(255) NOT NULL
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `user_registration`
@@ -110,7 +128,8 @@ CREATE TABLE IF NOT EXISTS `user_registration` (
 
 INSERT INTO `user_registration` (`id`, `userType`, `userName`, `userMobile`, `userEmail`, `userPass`, `status`, `remark`, `createdOn`, `createdBy`, `updatedOn`, `updatedBy`) VALUES
 (5, '2', 'Karan', '654567744655', 'karan@gmail.com', '123456', 1, '', '2023-06-02 05:33:39', 'Karan', '0000-00-00 00:00:00', ''),
-(4, '2', 'Mrityunjay Singh', '23654478958', 'ms@gmail.com', '12345', 0, '', '2023-05-31 20:39:58', 'Mrityunjay Singh', '0000-00-00 00:00:00', '');
+(4, '2', 'Mrityunjay Singh', '23654478958', 'ms@gmail.com', '12345', 2, 'Rejected', '2023-05-31 20:39:58', 'Mrityunjay Singh', '0000-00-00 00:00:00', ''),
+(6, '3', 'Rahul', '1654474987', 'rahul@gmail.com', '1111', 0, '', '2023-06-01 21:55:04', 'Rahul', '0000-00-00 00:00:00', '');
 
 -- --------------------------------------------------------
 
@@ -168,6 +187,12 @@ ALTER TABLE `business_category`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `customer_inquiry`
+--
+ALTER TABLE `customer_inquiry`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user_profile`
 --
 ALTER TABLE `user_profile`
@@ -201,15 +226,20 @@ ALTER TABLE `wall_uploads`
 ALTER TABLE `business_category`
 MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT for table `customer_inquiry`
+--
+ALTER TABLE `customer_inquiry`
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `user_profile`
 --
 ALTER TABLE `user_profile`
-MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `user_registration`
 --
 ALTER TABLE `user_registration`
-MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `user_type`
 --
