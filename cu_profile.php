715666277d166527f11f6548a982a9b0625b27e5
YashMaury <yasmaurya2000@gmail.com>
2023-06-08 15:56:00 +0530

    upd

---
+++ cu_profile.php
@@ -0,0 +1,77 @@
+<?php include "include/header.php"; ?>
+
+<section style="background-color: #eee;">
+                        <div class="container py-3">
+
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="card mb-4">
+                                        <div class="card-body text-center">
+                                            <img src="assets/img/events.png" alt="avatar"
+                                                class="rounded-circle img-fluid">
+                                            <h5 class="my-3">John Smith</h5>
+                                            <p class="text-muted mb-1">Full Stack Developer</p>
+                                            <p class="text-muted mb-1">example@example.com</p>
+                                            <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
+                                            <div class="d-flex justify-content-center mb-2">
+                                                <button type="button" class="btn btn-primary">Edit Proflie <i
+                                                        class="bi bi-pencil-square"></i></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-8">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <p class="mb-0">Full Name</p>
+                                                </div>
+                                                <div class="col-sm-9">
+                                                    <p class="text-muted mb-0"><input type="text" class="form-control"
+                                                            value="Johnatan Smith" disabled></p>
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <p class="mb-0">Email</p>
+                                                </div>
+                                                <div class="col-sm-9">
+                                                    <p class="text-muted mb-0">example@example.com</p>
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <p class="mb-0">Mobile</p>
+                                                </div>
+                                                <div class="col-sm-9">
+                                                    <p class="text-muted mb-0">(098) 765-4321</p>
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <p class="mb-0">Address</p>
+                                                </div>
+                                                <div class="col-sm-9">
+                                                    <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="d-flex justify-content-end mb-2">
+                                                    <button type="button" class="btn btn-success">Save Changes
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+<?php include "include/footer.php"; ?>
