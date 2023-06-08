0b5fa010241b50514fdb86d1238725c09ef5958c
Manu Computer <giplanand@gmail.com>
2023-06-08 17:24:36 +0530

    up

--- profile_list.php
+++ profile_list.php
@@ -13,7 +13,7 @@ curl_setopt($client, CURLOPT_POSTFIELDS, $postdata);
 $response = curl_exec($client);
 //print_r($response);
 $result = json_decode($response);
-//print_r($result);
+print_r($result);
 ?>
 <style>
     .rated {
