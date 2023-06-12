d0d0071821805033402109e461f3f01a22f117f1
mrityunjay570 <smrityunjay570@gmail.com>
2023-06-12 18:03:01 +0530

    updated

---
+++ admin/action/cu_profile_update_post.php
@@ -0,0 +1,52 @@
+<?php
+ include "../../constant.php";
+
+//  $userId='6';
+ $userId=$_POST["cuId"];
+ $businessName="na";
+ $businessCategory="na";
+//  $userAddress="add";
+ $userAddress=ucwords($_POST["cuAddress"]);
+ $state="na";
+ $city="na";
+ $alterMobile="na";
+ $establishmentYear="na";
+ $businessTiming="na";
+ $paymentMode ="na";
+ $businessDay ="na";
+ $userWebsite="na";
+ $aboutUser="na";
+ $updatedOn=date("Y-m-d");
+//  $updatedBy="rahul";
+ $updatedBy=$_POST["cuId"];
+  
+ $url = $URL . "user/update_user_profile.php";
+
+ $data = array("userId"=>$userId, "businessName"=>$businessName, "businessCategory"=>$businessCategory, "userAddress"=>$userAddress, "city"=>$city, "state"=>$state, "alterMobile"=>$alterMobile, "establishmentYear"=>$establishmentYear, "businessDay"=>$businessDay, "businessTiming"=>$businessTiming, "paymentMode"=>$paymentMode, "aboutUser"=>$aboutUser, "userWebsite"=>$userWebsite, "updatedOn"=>$updatedOn, "updatedBy"=>$updatedBy);
+
+  //print_r($data);
+
+ $postdata = json_encode($data);
+ $result=url_encode_Decode($url,$postdata);
+// print_r($result);
+//  if($result->message=="User profile updated successfully"){
+//   $_SESSION['profileupdate_success']="Updated successfully"; 
+//   header('location:../../profile.php');
+//  }else{
+//   header('location:../../update_user.php');
+//  }   
+
+    
+
+    function url_encode_Decode($url,$postdata){
+    $client = curl_init($url);
+    curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($client, CURLOPT_POSTFIELDS, $postdata);
+    $response = curl_exec($client);
+    print_r($response);
+    return $result = json_decode($response);
+
+}
+
+
+?>
--- cu_profile.php
+++ cu_profile.php
@@ -1,93 +1,168 @@
 <?php
 include "include/header.php";
-$userId = $_SESSION["USER_ID"];
-$url = $URL."user/read_user_profile.php";
-$userType='3';
-$status='1'; 
-$data = array("userType" =>$userType, "status"=>$status, "id"=>$userId);
+$userId = '6';
+// $userId = $_SESSION["USER_ID"];
+$url = $URL . "user/read_user_profile.php";
+$userType = '3';
+$status = '0';
+$data = array("userType" => $userType, "status" => $status, "id" => $userId);
 $postdata = json_encode($data);
 $client = curl_init($url);
-curl_setopt($client,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($client, CURLOPT_POSTFIELDS, $postdata);
 $response = curl_exec($client);
-print_r($response);
-$profile_result = json_decode($response);
-print_r($profile_result);
+//print_r($response);
+$result = json_decode($response);
+print_r($result);
 ?>
 
 <section>
-                        <div class="container py-3">
+    <div class="container py-3">
 
-                            <div class="row">
-                                <div class="col-lg-4">
-                                    <div class="card mb-4">
-                                        <div class="card-body text-center">
-                                            <img src="assets/img/events.png" alt="avatar"
-                                                class="rounded-circle img-fluid">
-                                            <h5 class="my-3">John Smith</h5>
-                                            <p class="text-muted mb-1">Full Stack Developer</p>
-                                            <p class="text-muted mb-1">example@example.com</p>
-                                            <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
-                                            <div class="d-flex justify-content-center mb-2">
-                                                <button type="button" class="btn btn-primary">Edit Proflie <i
-                                                        class="bi bi-pencil-square"></i></button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-8">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">Full Name</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0"><input type="text" class="form-control"
-                                                            value="Johnatan Smith" disabled></p>
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">Email</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">example@example.com</p>
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">Mobile</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">(098) 765-4321</p>
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">Address</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="row">
-                                                <div class="d-flex justify-content-end mb-2">
-                                                    <button type="button" class="btn btn-success">Save Changes
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+        <?php
+        $counter = '0';
+        foreach ($result as $key => $value) {
+            foreach ($value as $key1 => $value1) {
+                ?>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="card mb-4">
+                            <div class="card-body text-center">
+                                <img src="assets/img/events.png" alt="avatar" class="rounded-circle img-fluid">
+                                <h5 class="my-3">
+                                    <?php echo $value1->userName; ?>
+                                </h5>
+                                <!-- <p class="text-muted mb-1">Full Stack Developer</p> -->
+                                <p class="text-muted mb-1">
+                                    <?php echo $value1->userEmail; ?>
+                                </p>
+                                <div class="d-flex justify-content-center mb-2">
+                                    <button type="button" class="btn btn-primary" id="userEdit">Edit Proflie <i
+                                            class="bi bi-pencil-square"></i></button>
                                 </div>
                             </div>
                         </div>
-                    </section>
+                    </div>
 
-                    <hr>
+                    <div class="col-lg-8">
+                        <div class="card">
+                            <form method="post">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <p class="mb-0">Full Name</p>
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <p class="text-muted mb-0">
+                                                <input type="text" class="form-control" id="cuName"
+                                                    value="<?php echo $value1->userName; ?>" disabled>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <p class="mb-0">Email</p>
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <p class="text-muted mb-0">
+                                                <input type="text" class="form-control"
+                                                    value="<?php echo $value1->userEmail; ?>" disabled>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <p class="mb-0">Mobile</p>
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <p class="text-muted mb-0">
+                                                <input type="text" class="form-control" id="cuMobile"
+                                                    value="<?php echo $value1->userMobile; ?>" disabled>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <p class="mb-0">Address</p>
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <p class="text-muted mb-0">
+                                                <input class="form-control" type="text" id="cuAddress"
+                                                    value="<?php echo $value1->userAddress; ?>" id="userAdd" disabled>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <!-- <hr> -->
+                                    <div class="row">
+                                        <div class="d-flex justify-content-end mb-2">
+                                            <input type="hidden" value="<?php echo $value1->id; ?>" id="cuId">
+                                            <button type="button" id="editCustomer" style="display:none"
+                                                class="btn btn-success mt-2">Save Changes
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php }
+        } ?>
+</section>
+
+<hr>
+<script>
+    $(document).ready(function () {
+
+        $("#userEdit").click(function (event) {
+            event.preventDefault();
+            //    alert("clicked");
+            var cuName = $('#cuName').removeAttr("disabled").val();
+            var cuMobile = $('#cuMobile').removeAttr("disabled").val();
+            var cuAddress = $('#cuAddress').removeAttr("disabled").val();
+            $('#editCustomer').css("display", "block");
+        });
+
+        $("#editCustomer").click(function () {
+            var cuId = $('#cuId').val();
+            var cuName = $('#cuName').val();
+            var cuMobile = $('#cuMobile').val();
+            var cuAddress = $('#cuAddress').val();
+            // alert(cuId);
+            $.ajax({
+                url: "<?php echo $BASE_URL ?>admin/action/cu_profile_update_post.php",
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    cuId: cuId,
+                    cuName: cuName,
+                    cuMobile: cuMobile,
+                    cuAddress: cuAddress
+                },
+                success: function (response) {
+                    // alert(response);
+                    // alert(JSON.stringify(response));
+                    if(response.message=="User profile updated successfully"){
+                        // alert("done");
+                        // $('#cuUpdateMsg').text("Hello world!");
+                    // $("#cuUpdateMsg").fadeTo(2000, 500).slideUp(500, function () {
+                    // $("#cuUpdateMsg").slideUp(500);
+                    //     });
+                     
+                    
+                    }
+                   
+
+                }
+
+            });
+
+        });
+
+    });
+</script>
 <?php include "include/footer.php"; ?>
