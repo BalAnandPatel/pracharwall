13569f955e680c0f0630b1fe0d4b57752fbdcc2a
BalAnandPatel <giplanand@gmail.com>
2023-08-04 17:53:58 +0530

    up

--- admin/action/user_login_post.php
+++ admin/action/user_login_post.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 error_reporting(0);
 include '../../constant.php';
 include '../../common/php-jwt/src/JWT.php';
